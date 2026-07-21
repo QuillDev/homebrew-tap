109dda0f283f3ff25b82ed7c65a3439de45c4bae
robert-brunson-fragile <siinspam@gmail.com>
2026-07-20 17:09:08 -0700

    Update Nakode to 0.2.0

--- Formula/nakode.rb
+++ Formula/nakode.rb
@@ -1,8 +1,8 @@
 class Nakode < Formula
   desc "Provider-neutral terminal workspace for coding agents"
   homepage "https://github.com/QuillDev/nakode"
-  url "https://github.com/QuillDev/nakode/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "219dcce4b912fe043aab971b5da187f20e3d6f9704775e57315bc6e2fb554bfc"
+  url "https://github.com/QuillDev/nakode/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "e51390d803a41e2409afa7dff1ab9c2d39a649b711e66d88bee5691a4edf06cf"
   head "https://github.com/QuillDev/nakode.git", branch: "main"
 
   depends_on "rust" => :build
